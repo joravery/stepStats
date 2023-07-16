3621fd277e01b3c4359f42400e2f6ec7e812bea0
🤘🚀joravery@ <jordan.avery1@gmail.com>
2023-07-16 11:01:50 -0700

    Cleanup lambda build and dev requirements

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-rm deployment.zip
-mkdir -p lambda_build
+rm -f deployment.zip
+mkdir lambda_build
 cd lambda_build
-cp ../lambda_function.py .
 pip3 install --target . -r ../lambda_requirements.txt
 zip -r ../deployment.zip ./*
 cd ../
 zip -r deployment.zip util
-zip -r deployment.zip lambda_function.py
+zip deployment.zip lambda_function.py
 cp deployment.zip ../
-cd ../
 rm -rf lambda_build
